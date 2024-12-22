4d4a6f41fd7b34ca701c043335dfe4538978ba88
Matheus Scramignon <matheus@hybridai.com.br>
2024-12-22 14:26:32 -0300

    Lint .py files using autopep8

--- Predict Customer Churn with Clean Code/source/churn_library.py
+++ Predict Customer Churn with Clean Code/source/churn_library.py
@@ -2,25 +2,24 @@
 
 
 # import libraries
-import os
-os.environ['QT_QPA_PLATFORM']='offscreen'
-
-import shap
-import joblib
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-
-from sklearn.preprocessing import normalize
-from sklearn.model_selection import train_test_split
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-
-from sklearn.metrics import plot_roc_curve, classification_report
+import seaborn as sns
 from source.constants import *
+from sklearn.metrics import plot_roc_curve, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import joblib
+import shap
+import os
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+
+sns.set()
+
 
 def import_data(pth):
     '''
@@ -30,7 +29,7 @@ def import_data(pth):
             pth: a path to the csv
     output:
             df: pandas dataframe
-    '''	
+    '''
     df = pd.read_csv(pth)
 
     return df
@@ -46,32 +45,32 @@ def perform_eda(df):
             None
     '''
     # Save Churn Histogram plot
-    fig = plt.figure(figsize=(20,10)) 
+    fig = plt.figure(figsize=(20, 10))
     ax = df['Churn'].hist()
     ax.set_title('Churn Histogram')
     fig.savefig(IMAGES_PATH / 'churn_histogram.png')
 
     # Save Customer Age Histogram plot
-    fig = plt.figure(figsize=(20,10)) 
+    fig = plt.figure(figsize=(20, 10))
     ax = df['Customer_Age'].hist()
     ax.set_title('Customer Age Histogram')
     fig.savefig(IMAGES_PATH / 'customer_age.png')
 
     # Save Marital Status Bar plot
-    fig = plt.figure(figsize=(20,10)) 
+    fig = plt.figure(figsize=(20, 10))
     ax = df.Marital_Status.value_counts('normalize').plot(kind='bar')
     ax.set_title('Marital Status Bar Plot (Normalized)')
     fig.savefig(IMAGES_PATH / 'marital_status.png')
 
     # Save Total Transactions Histogram plot
-    fig = plt.figure(figsize=(20,10)) 
+    fig = plt.figure(figsize=(20, 10))
     ax = sns.histplot(df['Total_Trans_Ct'], stat='density', kde=True)
     ax.set_title('Total Transactions Histogram (KDE)')
     fig.savefig(IMAGES_PATH / 'total_trans.png')
 
     # Save Heat Map
-    fig = plt.figure(figsize=(20,10)) 
-    ax = sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+    fig = plt.figure(figsize=(20, 10))
+    ax = sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths=2)
     ax.set_title('Heat Map')
     fig.savefig(IMAGES_PATH / 'heat_map.png')
 
@@ -118,8 +117,9 @@ def perform_feature_engineering(df, response='Churn'):
     X_columns = [col for col in df.columns.tolist() if col != response]
     X, y = df[X_columns], df[response]
 
-    # train test split 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)
+    # train test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
 
     return X_train, X_test, y_train, y_test
 
@@ -149,26 +149,38 @@ def classification_report_image(y_train,
     report_rf_test = classification_report(y_test, y_test_preds_rf)
     print(report_rf_test)
 
-    plot_classification_report(report_rf_test, IMAGES_PATH, 'report_rf_test.png')
+    plot_classification_report(
+        report_rf_test,
+        IMAGES_PATH,
+        'report_rf_test.png')
 
     print('train results')
     report_rf_train = classification_report(y_train, y_train_preds_rf)
     print(report_rf_train)
 
-    plot_classification_report(report_rf_train, IMAGES_PATH, 'report_rf_train.png')
+    plot_classification_report(
+        report_rf_train,
+        IMAGES_PATH,
+        'report_rf_train.png')
 
     print('logistic regression results')
     print('test results')
     report_lr_test = classification_report(y_test, y_test_preds_lr)
     print(report_lr_test)
 
-    plot_classification_report(report_lr_test, IMAGES_PATH, 'report_lr_test.png')
+    plot_classification_report(
+        report_lr_test,
+        IMAGES_PATH,
+        'report_lr_test.png')
 
     print('train results')
     report_lr_train = classification_report(y_train, y_train_preds_lr)
     print(report_lr_train)
 
-    plot_classification_report(report_lr_train, IMAGES_PATH, 'report_lr_train.png')
+    plot_classification_report(
+        report_lr_train,
+        IMAGES_PATH,
+        'report_lr_train.png')
 
 
 def plot_classification_report(report, output_folder, filename):
@@ -187,7 +199,7 @@ def plot_classification_report(report, output_folder, filename):
     lines = report.split('\n')
     classes = []
     plot_data = []
-    
+
     # for line in lines[2:-3]:
     for line in lines[2:]:
         tokens = line.split()
@@ -195,11 +207,11 @@ def plot_classification_report(report, output_folder, filename):
         if len(tokens) < 2:
             continue
         classes.append(tokens[0])
-        
+
         if classes[-1] == 'accuracy':
             tokens.insert(1, '0.0')
             tokens.insert(1, '0.0')
-            
+
         data_line = [float(x) for x in tokens[1:] if x != 'avg']
         plot_data.append(data_line)
 
@@ -209,8 +221,15 @@ def plot_classification_report(report, output_folder, filename):
 
     # Create a heatmap plot
     plt.figure(figsize=(10, len(classes) * 0.6))
-    sns.heatmap(data[:, :-1], annot=True, fmt=".2f", cmap="coolwarm", cbar=True,
-                xticklabels=metrics[:-1], yticklabels=classes, linewidths=0.5)
+    sns.heatmap(data[:,
+                     :-1],
+                annot=True,
+                fmt=".2f",
+                cmap="coolwarm",
+                cbar=True,
+                xticklabels=metrics[:-1],
+                yticklabels=classes,
+                linewidths=0.5)
 
     plt.title("Classification Report", fontsize=16)
     plt.xlabel("Metrics", fontsize=12)
@@ -244,11 +263,11 @@ def roc_curves_plot(model_rf, model_lr, X_data, y_data):
     fig, ax = plt.subplots()
     rf_plot = plot_roc_curve(model_rf, X_data, y_data, ax=ax)
     fig.savefig(IMAGES_PATH / 'roc_random_forest.png')
-    
+
     fig, ax = plt.subplots()
     lrc_plot = plot_roc_curve(model_lr, X_data, y_data, ax=ax)
     fig.savefig(IMAGES_PATH / 'roc_logistic_regression.png')
-    
+
     fig, ax = plt.subplots(figsize=(15, 8))
     rfc_disp = plot_roc_curve(model_rf, X_data, y_data, ax=ax, alpha=0.8)
     lrc_plot.plot(ax=ax, alpha=0.8)
@@ -267,7 +286,7 @@ def feature_importance_plot(model, X_data):
     '''
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(X_data)
-    
+
     fig = plt.figure()
     shap.summary_plot(shap_values, X_data, plot_type="bar")
     ax = plt.gca()
@@ -290,11 +309,11 @@ def train_models(X_train, X_test, y_train, y_test):
     rfc = RandomForestClassifier(random_state=42)
     lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
 
-    param_grid = { 
+    param_grid = {
         'n_estimators': [200, 500],
         'max_features': ['auto', 'sqrt'],
-        'max_depth' : [4,5,100],
-        'criterion' :['gini', 'entropy']
+        'max_depth': [4, 5, 100],
+        'criterion': ['gini', 'entropy']
     }
 
     cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
@@ -309,7 +328,13 @@ def train_models(X_train, X_test, y_train, y_test):
     y_test_preds_lr = lrc.predict(X_test)
 
     # Generate classification reports images
-    classification_report_image(y_train, y_test, y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf)
+    classification_report_image(
+        y_train,
+        y_test,
+        y_train_preds_lr,
+        y_train_preds_rf,
+        y_test_preds_lr,
+        y_test_preds_rf)
 
     # Generate features importance plot
     feature_importance_plot(cv_rfc.best_estimator_, X_test)
@@ -329,7 +354,8 @@ if __name__ == "__main__":
     df = import_data((DATA_PATH / 'bank_data.csv').as_posix())
 
     # Create 'Churn' column
-    df[TARGET_COLUMN] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    df[TARGET_COLUMN] = df['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1)
 
     # Perform EDA
     perform_eda(df)
@@ -338,7 +364,8 @@ if __name__ == "__main__":
     df = encoder_helper(df, CAT_COLUMNS, TARGET_COLUMN)
 
     # Perform feature engineering
-    X_train, X_test, y_train, y_test = perform_feature_engineering(df[KEEP_COLUMNS + [TARGET_COLUMN]], TARGET_COLUMN)
+    X_train, X_test, y_train, y_test = perform_feature_engineering(
+        df[KEEP_COLUMNS + [TARGET_COLUMN]], TARGET_COLUMN)
 
     # Train and save models related files
     train_models(X_train, X_test, y_train, y_test)
--- Predict Customer Churn with Clean Code/source/churn_script_logging_and_tests.py
+++ Predict Customer Churn with Clean Code/source/churn_script_logging_and_tests.py
@@ -6,202 +6,206 @@ from source.constants import *
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
 
 @pytest.fixture()
 def df():
-	try:
-		df = import_data((DATA_PATH / 'bank_data.csv').as_posix())
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
-	
-	return df
+    try:
+        df = import_data((DATA_PATH / 'bank_data.csv').as_posix())
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
+
+    return df
 
 
 @pytest.fixture()
 def df_churn(df):
-	# Create 'Churn' column
-	df_churn = df.copy()
-	df_churn[TARGET_COLUMN] = df_churn['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    # Create 'Churn' column
+    df_churn = df.copy()
+    df_churn[TARGET_COLUMN] = df_churn['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1)
 
-	return df_churn
+    return df_churn
 
 
 @pytest.fixture()
 def df_encoded(df_churn):
-	df_encoded = df_churn.copy()
+    df_encoded = df_churn.copy()
 
-    # Call the encoder_helper function
-	df_encoded = encoder_helper(df_churn, CAT_COLUMNS, TARGET_COLUMN)
+# Call the encoder_helper function
+    df_encoded = encoder_helper(df_churn, CAT_COLUMNS, TARGET_COLUMN)
 
-	return df_encoded
+    return df_encoded
 
 
 @pytest.fixture()
 def df_featurized(df_encoded):
-	df_featurized = df_encoded.copy()
-	df_featurized = df_featurized[KEEP_COLUMNS + [TARGET_COLUMN]]
+    df_featurized = df_encoded.copy()
+    df_featurized = df_featurized[KEEP_COLUMNS + [TARGET_COLUMN]]
 
-	return df_featurized
+    return df_featurized
 
 
 def test_import(df):
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+    '''
+    test data import - this example is completed for you to assist with the other test functions
+    '''
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
 
 
 def test_eda(df_churn):
-	'''
-	test perform eda function
-	'''
-	# Ensure the images folder exists
-	assert IMAGES_PATH.exists()
-	logging.info("Testing perform_eda: IMAGES_PATH exists")
+    '''
+    test perform eda function
+    '''
+    # Ensure the images folder exists
+    assert IMAGES_PATH.exists()
+    logging.info("Testing perform_eda: IMAGES_PATH exists")
 
-    # Call the perform_eda function
-	perform_eda(df_churn)
+# Call the perform_eda function
+    perform_eda(df_churn)
 
-    # Check if the files are created
-	expected_files = [
-		'churn_histogram.png',
-		'customer_age.png',
-		'marital_status.png',
-		'total_trans.png',
-		'heat_map.png'
-	]
+# Check if the files are created
+    expected_files = [
+        'churn_histogram.png',
+        'customer_age.png',
+        'marital_status.png',
+        'total_trans.png',
+        'heat_map.png'
+    ]
 
-	try:
-		for file in expected_files:
-			assert (IMAGES_PATH / file).exists()
-	except AssertionError as err:
-		logging.error(f"Testing perform_eda: File {file} was not created.")
-		raise err
+    try:
+        for file in expected_files:
+            assert (IMAGES_PATH / file).exists()
+    except AssertionError as err:
+        logging.error(f"Testing perform_eda: File {file} was not created.")
+        raise err
 
 
 def test_encoder_helper(df_encoded):
-	'''
-	test encoder helper
-	'''
-	category_lst = CAT_COLUMNS
-	response = TARGET_COLUMN
+    '''
+    test encoder helper
+    '''
+    category_lst = CAT_COLUMNS
+    response = TARGET_COLUMN
 
-    # Check if the new columns are created
-	try:
-		for col in category_lst:
-			new_col = f'{col}_{response}'
-			assert new_col in df_encoded.columns
-	except AssertionError as err:
-		logging.error(f"Testing encoder_helper: Column {new_col} was not created.")
-		raise err
-	
-	logging.info("Testing encoder_helper: New columns successfully created.")
+# Check if the new columns are created
+    try:
+        for col in category_lst:
+            new_col = f'{col}_{response}'
+            assert new_col in df_encoded.columns
+    except AssertionError as err:
+        logging.error(
+            f"Testing encoder_helper: Column {new_col} was not created.")
+        raise err
 
-    # Verify the new column values
-	try:
-		for col in category_lst:
-			new_col = f'{col}_{response}'
-			assert not df_encoded[new_col].isnull().any()
-	except AssertionError as err:
-		logging.error(f"Testing encoder_helper: Column {new_col} contains null values.")
-		raise err
-	
-	logging.info("Testing encoder_helper: New columns created with sound values.")
+    logging.info("Testing encoder_helper: New columns successfully created.")
+
+# Verify the new column values
+    try:
+        for col in category_lst:
+            new_col = f'{col}_{response}'
+            assert not df_encoded[new_col].isnull().any()
+    except AssertionError as err:
+        logging.error(
+            f"Testing encoder_helper: Column {new_col} contains null values.")
+        raise err
+
+    logging.info(
+        "Testing encoder_helper: New columns created with sound values.")
 
 
 def test_perform_feature_engineering(df_featurized):
-	'''
-	test perform_feature_engineering
-	'''
-	# Call the perform_feature_engineering function
-	X_train, X_test, y_train, y_test = perform_feature_engineering(df_featurized, response=TARGET_COLUMN)
+    '''
+    test perform_feature_engineering
+    '''
+    # Call the perform_feature_engineering function
+    X_train, X_test, y_train, y_test = perform_feature_engineering(
+        df_featurized, response=TARGET_COLUMN)
 
-    # Check if the splits are correct
-	try:
-		assert len(X_train) > 0
-		assert len(X_test) > 0
-		assert len(y_train) > 0
-		assert len(y_test) > 0
-	except AssertionError as err:
-		logging.error("Testing perform_feature_engineering: One of the returned sets is empty")
-		raise err
-	
-	logging.info("Testing perform_feature_engineering: Returned sets are not empty")
+# Check if the splits are correct
+    try:
+        assert len(X_train) > 0
+        assert len(X_test) > 0
+        assert len(y_train) > 0
+        assert len(y_test) > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing perform_feature_engineering: One of the returned sets is empty")
+        raise err
 
-    # Check if the shapes match
-	try:
-		assert X_train.shape[1] == X_test.shape[1]
-	except AssertionError as err:
-		logging.error("Testing perform_feature_engineering: Mismatch in number of features between X_train and X_test.")
-		raise err
-	
-	logging.info("Testing perform_feature_engineering: Sets successfully returned by function.")
+    logging.info(
+        "Testing perform_feature_engineering: Returned sets are not empty")
+
+# Check if the shapes match
+    try:
+        assert X_train.shape[1] == X_test.shape[1]
+    except AssertionError as err:
+        logging.error(
+            "Testing perform_feature_engineering: Mismatch in number of features between X_train and X_test.")
+        raise err
+
+    logging.info(
+        "Testing perform_feature_engineering: Sets successfully returned by function.")
 
 
 def test_train_models(df_featurized):
-	'''
-	test train_models
-	'''
-	X_train, X_test, y_train, y_test = perform_feature_engineering(df_featurized, response=TARGET_COLUMN)
+    '''
+    test train_models
+    '''
+    X_train, X_test, y_train, y_test = perform_feature_engineering(
+        df_featurized, response=TARGET_COLUMN)
 
-    # Ensure directories exist
-	# Ensure the models folder exists
-	assert MODELS_PATH.exists()
-	logging.info("Testing train_models: MODELS_PATH exists.")
+# Ensure directories exist
+    # Ensure the models folder exists
+    assert MODELS_PATH.exists()
+    logging.info("Testing train_models: MODELS_PATH exists.")
 
-    # Call the train_models function
-	train_models(X_train, X_test, y_train, y_test)
+# Call the train_models function
+    train_models(X_train, X_test, y_train, y_test)
 
-	# Check if the images files are created
-	expected_images = [
+    # Check if the images files are created
+    expected_images = [
         'report_rf_test.png',
         'report_rf_train.png',
         'report_lr_test.png',
         'report_lr_train.png',
         'roc_random_forest.png',
-		'roc_logistic_regression.png',
-		'roc_curves.png',
-		'features_importance.png',
+        'roc_logistic_regression.png',
+        'roc_curves.png',
+        'features_importance.png',
     ]
 
-	try:
-		for file in expected_images:
-			assert (IMAGES_PATH / file).exists()
-	except AssertionError as err:
-		logging.error(f"Testing train_models: File {file} was not created.")
-		raise err
-	
-	logging.info("Testing train_models: Images files successfully created.")
+    try:
+        for file in expected_images:
+            assert (IMAGES_PATH / file).exists()
+    except AssertionError as err:
+        logging.error(f"Testing train_models: File {file} was not created.")
+        raise err
 
-    # Check if models are saved
-	try:
-		assert (MODELS_PATH / 'rfc_model.joblib').exists()
-		assert (MODELS_PATH / 'logistic_model.joblib').exists()
-	except AssertionError as err:
-		logging.error(f"Testing train_models: One or more models were not created.")
-		raise err
-	
-	logging.info("Testing train_models: Models successfully created.")
+    logging.info("Testing train_models: Images files successfully created.")
+
+# Check if models are saved
+    try:
+        assert (MODELS_PATH / 'rfc_model.joblib').exists()
+        assert (MODELS_PATH / 'logistic_model.joblib').exists()
+    except AssertionError as err:
+        logging.error(
+            f"Testing train_models: One or more models were not created.")
+        raise err
+
+    logging.info("Testing train_models: Models successfully created.")
 
 
 if __name__ == "__main__":
-	pass
-
-
-
-
-
-
-
-
+    pass
